8c52b0c6ffec5ab49d973221ab4be1f90230ffb2
MARYIA STUPEN <Maryia_Stupen@epam.com>
2017-08-14 13:01:56 +0300

    "test fix"

--- test/step_definitions/projectPageSteps.js
+++ test/step_definitions/projectPageSteps.js
@@ -3,7 +3,7 @@ var {defineSupportCode} = require('cucumber');
 defineSupportCode(function({Given, When, Then}) {
 
 	When(/^I have my project created with the name '([^']*)', the project description '([^']*)' and visible internally$/, function (projectName, projectDescription) {
-    return this.pageFactory.currentPage.checkProjectParameters();
+    return this.pageFactory.currentPage.checkProjectParameters(projectName, projectDescription);
   });
 
 	When(/^I remove an empty project$/, function () {
