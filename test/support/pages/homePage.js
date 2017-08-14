56bec2dc2698fea3494a4a17d4380dda525da907
MARYIA STUPEN <Maryia_Stupen@epam.com>
2017-08-14 15:31:08 +0300

    deleted some useless files from the previous project

--- test/support/pages/homePage.js
+++ test/support/pages/homePage.js
@@ -18,27 +18,6 @@ var homePage = function(world) {
 			projectName: {
 				css: '.project-name',
 				isSingle: false
-			},
-			accountLink: {
-				css: '.navbar__account',
-				text: 'My account',
-				isSingle: true
-			},
-			loginDetailsLink: {
-				css: '.navbar__account-dropdown li:nth-child(8) a',
-				isSingle: true
-			},
-			loginButton: {
-				css: '.button[data-synthetics="header-account-dropdown-login"]',
-				isSingle: true
-			},
-			searchField: {
-				css: 'input.navbar__search-field',
-				isSingle: true
-			},
-			searchButton: {
-				css: 'button.button.button--icon',
-				isSingle: true
 			}
 		}
 	};
@@ -62,34 +41,6 @@ var homePage = function(world) {
 			});
 	};
 
-	_this.clickOnAccountLink = function() {
-		return _this.world.helper.elementGetter(_this._root, _this._data.elements.accountLink).waitReady()
-			.then((element) => {
-				return element.click();
-			});
-	};
-	_this.clickOnLoginButton = function() {
-		return _this.world.helper.elementGetter(_this._root, _this._data.elements.loginButton).click()
-			.then(() => {
-				return _this.world.browserUtils.changePage('login');
-			});
-	};
-
-	_this.makeQuery = function(query) {
-		return _this.world.helper.elementGetter(_this._root, _this._data.elements.searchField).sendKeys(query).then(() => {}).then(() => {
-			return _this.world.helper.elementGetter(_this._root, _this._data.elements.searchButton).click();
-		}).then(() => {
-			return _this.world.browserUtils.changePage('query-results');
-		});
-	};
-	_this.clickOnLoginDetailsLink = function() {
-		return _this.world.helper.elementGetter(_this._root, _this._data.elements.loginDetailsLink).waitReady()
-			.then((element) => {
-				element.click();
-			}).then(() => {
-				return _this.world.browserUtils.changePage('login-details')
-			});
-	};
 };
 
 inheritance.inherits(Page, homePage);
