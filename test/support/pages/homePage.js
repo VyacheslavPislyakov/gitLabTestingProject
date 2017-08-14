f7aa7f253fe6594aebdbaf7032a5705c9d26a6aa
MARYIA STUPEN <Maryia_Stupen@epam.com>
2017-08-14 15:27:47 +0300

    fixed getText() function implementation

--- test/support/pages/homePage.js
+++ test/support/pages/homePage.js
@@ -53,13 +53,12 @@ var homePage = function(world) {
 	_this.checkIfValueIsPresentInTab = function(value) {
 		return browser.wait(EC.presenceOf(_this.world.helper.elementGetter(_this._root, _this._data.elements.projectName)), 5000)
 			.then(() => {
-				return _this.world.helper.elementGetter(_this._root, _this._data.elements.projectName)
-			}).then((elements) => {
-				for (var i = 0; i < elements.length; i++) {
-					///SOMETHING IS WRONG
-					console.log(elements[i].getText());
-					if (elements[i].getText() === value) return "Project has not been deleted. Tab contains project's name"
-				}
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements.projectName).each(element =>{
+					element.getText()
+						.then(function (text) {
+	    				expect(text).not.to.equal(value);
+	  			});
+				});
 			});
 	};
 
