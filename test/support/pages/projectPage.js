237cd907483cedc8b379e477c67e4ee259bf8a4d
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2017-08-15 10:44:01 +0300

    create test of visibility of the project

--- test/support/pages/projectPage.js
+++ test/support/pages/projectPage.js
@@ -1,88 +1,111 @@
 var inheritance = require('./../helpers/inheritance'),
-    Page = require('./page');
+	Page = require('./page');
 
-var projectPage = function(world){
-  var _this=this;
-  _this.world = world;
-  _this.marker = 'project';
+var projectPage = function(world) {
+	var _this = this;
+	_this.world = world;
+	_this.marker = 'project';
 
-  _this._data = {
-    elements: {
-      projectTitle: {
+	_this._data = {
+		elements: {
+			projectTitle: {
 				css: '.project-title',
 				isSingle: true
-		  },
-      projectDescription: {
- 				css: '.project-home-desc > p',
- 				isSingle: true
- 		  },
-      projectDescription: {
- 				css: '.project-home-desc > p',
- 				isSingle: true
- 		  },
-      shieldIcon: {
-        css: '.fa-shield',
-        isSingle: true
-      },
-      removeProjectButton: {
-        css: '.btn-remove',
-        isSingle: true
-      },
-      readmeLink: {
-        css: 'a.underlined-link',
-        text: 'README',
-        isSingle: true
-      },
-      repository: {
-        css: 'a[title="Repository"]',
-        isSingle: true
-      },
-      issues: {
-        css: '.shortcuts-issues',
-        isSingle: true
-      }
-   }
-  };
+			},
+			projectDescription: {
+				css: '.project-home-desc > p',
+				isSingle: true
+			},
+			projectDescription: {
+				css: '.project-home-desc > p',
+				isSingle: true
+			},
+			shieldIcon: {
+				css: '.fa-shield',
+				isSingle: true
+			},
+            privateIcon: {
+				css: '.fa.fa-lock',
+				isSingle: true
+			},
+			removeProjectButton: {
+				css: '.btn-remove',
+				isSingle: true
+			},
+			readmeLink: {
+				css: 'a.underlined-link',
+				text: 'README',
+				isSingle: true
+			},
+			repository: {
+				css: 'a[title="Repository"]',
+				isSingle: true
+			},
+			issues: {
+				css: '.shortcuts-issues',
+				isSingle: true
+			},
+			settingsTab: {
+				css: 'span',
+				text: 'Settings',
+				isSingle: true
+			}
+		}
+	};
 
-  _this.checkProjectParameters = function(expectedTitle,expectedDescription){
-    return browser.wait(EC.visibilityOf(_this.world.helper.elementGetter(_this._root,_this._data.elements.projectTitle)), 5000)
-      .then(()=>{
-        return _this.world.helper.elementGetter(_this._root,_this._data.elements.projectTitle).getText();
-      }).then((title)=>{
-        return expect(title).to.equal(expectedTitle);
-      }).then(()=>{
-        return _this.world.helper.elementGetter(_this._root,_this._data.elements.projectDescription).getText();
-      }).then((description)=>{
-        return expect(description).to.equal(expectedDescription);
-      }).then(()=>{
-        return browser.wait(EC.visibilityOf(_this.world.helper.elementGetter(_this._root,_this._data.elements.shieldIcon)), 5000)
-      },(error)=>{
-        console.log("Shield icon is not visible");
-      });
-  };
-  _this.removeAnEmptyProject = function(){
-    return browser.wait(EC.presenceOf(_this.world.helper.elementGetter(_this._root,_this._data.elements.removeProjectButton)), 5000)
-      .then(()=>{
-         return _this.world.helper.elementGetter(_this._root,_this._data.elements.removeProjectButton).scrollIntoView();
-      }).then(()=>{
-        return _this.world.helper.elementGetter(_this._root,_this._data.elements.removeProjectButton).click();
-      }).then(()=>{
-        return browser.wait(EC.alertIsPresent(), 5000);
-      }).then(()=>{
-      browser.switchTo().alert().accept();
-      });
-  };
+	_this.checkProjectParameters = function(expectedTitle, expectedDescription) {
+		return browser.wait(EC.visibilityOf(_this.world.helper.elementGetter(_this._root, _this._data.elements.projectTitle)), 5000)
+			.then(() => {
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements.projectTitle).getText();
+			}).then((title) => {
+				return expect(title).to.equal(expectedTitle);
+			}).then(() => {
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements.projectDescription).getText();
+			}).then((description) => {
+				return expect(description).to.equal(expectedDescription);
+			}).then(() => {
+				return browser.wait(EC.visibilityOf(_this.world.helper.elementGetter(_this._root, _this._data.elements.shieldIcon)), 5000)
+			}, (error) => {
+				console.log("Shield icon is not visible");
+			});
+	};
 
-  _this.clickOn = function(elementName){
-    return browser.wait(EC.presenceOf(_this.world.helper.elementGetter(_this._root,_this._data.elements[elementName])), 10000)
-      .then(()=>{
-        return _this.world.helper.elementGetter(_this._root,_this._data.elements[elementName]).click();
-      });
-  };
+    _this.checkProjectVisibility = function(expectedTitle, expectedDescription) {
+		return browser.wait(EC.visibilityOf(_this.world.helper.elementGetter(_this._root, _this._data.elements.projectTitle)), 5000)
+			.then(() => {
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements.projectTitle).getText();
+			}).then((title) => {
+				return expect(title).to.equal(expectedTitle);
+			}).then(() => {
+				return browser.wait(EC.visibilityOf(_this.world.helper.elementGetter(_this._root, _this._data.elements.shieldIcon)), 5000)
+			}, (error) => {
+				console.log("Shield icon is not visible");
+			});
+	};
+
+	_this.removeAnEmptyProject = function() {
+		return browser.wait(EC.presenceOf(_this.world.helper.elementGetter(_this._root, _this._data.elements.removeProjectButton)), 5000)
+			.then(() => {
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements.removeProjectButton).scrollIntoView();
+			}).then(() => {
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements.removeProjectButton).click();
+			}).then(() => {
+				return browser.wait(EC.alertIsPresent(), 5000);
+			}).then(() => {
+				browser.switchTo().alert().accept();
+			});
+	};
+
+	_this.clickOn = function(elementName) {
+		return browser.wait(EC.presenceOf(_this.world.helper.elementGetter(_this._root, _this._data.elements[elementName])), 10000)
+			.then(() => {
+				return _this.world.helper.elementGetter(_this._root, _this._data.elements[elementName]).click();
+			});
+	};
 
 
 };
 
-inheritance.inherits(Page,projectPage);
+inheritance.inherits(Page, projectPage);
 
 module.exports = projectPage;
