6da3ffa52b4805693568f1e96217b4bde33bc9e9
MARYIA STUPEN <Maryia_Stupen@epam.com>
2017-08-21 16:45:46 +0300

    SHIFT EXECUTION TO BEFOREALL FUNCTION

--- test/support/hooks.js
+++ test/support/hooks.js
@@ -8,6 +8,14 @@ defineSupportCode(function({Before,	After, setDefaultTimeout}) {
 		return browser.driver.manage().window().maximize();
 	});
 
+	BeforeAll(function() {
+		exec('./login.exe', [process.env.MY_USER, process.env.MY_PASS], function(err, data) {
+			console.log(process.env.MY_USER.toString());
+			console.log(data.toString());
+		});
+		return browser.sleep(10000);
+	});
+
 	After(function() {
         return browser.executeScript('window.localStorage.clear();')
         .then(function () {
